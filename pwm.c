ab4c239fca93fd0d1cdd3422136495eae66c11c4
Alexander Mohn <pogrmman@gmail.com>
2019-01-07 22:07:38 -0600

    Initial working version

--- pwm.c
+++ pwm.c
@@ -2,20 +2,21 @@
 #include <avr/io.h>
 #include <util/delay.h>
 int main() {
-  // Set pip 6 to output
-  DDRB = (1<<DDB1);
+  // Set pin 5 to output
+  DDRB = (1<<PB0);
 
   // Set up PWM
+  OCR0A = 20; // Duty cycle
   // Run it in normal mode, not inverted
   TCCR0A = ((1<<COM0A1)|(0<<COM0A0));
   // Fast PWM mode
   TCCR0A |= ((1<<WGM01)|(1<<WGM00));
   TCCR0B = (0<<WGM02);
-  // Clock has no prescaler
-  TCCR0B |= (1<<CS00);
+  // Clock has 1/8 prescaler
+  TCCR0B |= (1<<CS01);
   
   while(1) {
-    OCR0A = 255;
+    ;
   }
 
   return 0;
